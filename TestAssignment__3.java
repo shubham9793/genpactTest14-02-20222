d64bf2a9f36fb9931089b0bd5f6c7f09f00bff8c
shubham sahoo <56964646+shubham9793@users.noreply.github.com>
2022-02-14 13:50:20 +0530

    Update TestAssignment__3.java

--- TestAssignment__3.java
+++ TestAssignment__3.java
@@ -1,62 +1,59 @@
 package com.org.gen.day6;
 
+class Bank {
 
-class Bank{
+   int getBalance() {
 
-	int getBalance() { 
-		
-			return 0; 
-		}
-	}
+      return 0;
+   }
+}
 
+class BankA extends Bank {
 
-class BankA extends Bank{
+   int getBalance(int balance) {
 
-	int getBalance(int balance) {
-		
-			return balance; 
-		}
+      return balance;
+   }
 
 }
 
 class BankB extends Bank {
 
-	int getBalance(int balance) { 
-		
-		return balance;
-		
-		}
+   int getBalance(int balance) {
+
+      return balance;
+
+   }
 
 }
 
-class BankC extends Bank{
+class BankC extends Bank {
 
-	
-int getBalance(int balance) {
-	
-	return balance; 
-	
-	}
+   int getBalance(int balance) {
+
+      return balance;
+
+   }
 
 }
 
-public class TestAssignment__3{
+public class TestAssignment__3 {
 
-	public static void main(String args[]) {
-		// TODO Auto-generated method stub
+   public static void main(String args[]) {
+      // TODO Auto-generated method stub
 
-		BankA a = new BankA();
+      BankA a = new BankA();
 
-		BankB b = new BankB();
-		
-		BankC c = new BankC();
+      BankB b = new BankB();
 
-		System.out.println("Bank A " +"$"+a.getBalance(1000));
+      BankC c = new BankC();
 
-		System.out.println("Bank B " +"$"+b.getBalance(1500));
+      System.out.println("Bank A " + "$" + a.getBalance(1000));
 
-		System.out.println("Bank C " +"$"+c.getBalance(2000));
+      System.out.println("Bank B " + "$" + b.getBalance(1500));
 
-	}
+      System.out.println("Bank C " + "$" + c.getBalance(2000));
+
+   }
 
 }
